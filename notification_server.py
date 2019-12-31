320ea3ce60ab957208d04d01aeb59cbd18c02f01
www.MyPiDrone.com <philippe.le-coq@laposte.net>
2019-12-31 15:42:23 +0100

    Update notification_server.py

--- notification_server.py
+++ notification_server.py
@@ -8,6 +8,9 @@
 # Create  /root/daemon_server/notification_server.py
 # Create /root/daemon_server/notification_client.py
 #-------------------------------------------------------------
+# Change port 8085 if you want in code here notification_server.py 
+# and here notification_client.py
+#-------------------------------------------------------------
 # Create /etc/systemd/system/notification_server.service
 # systemctl enable notification_server
 # systemctl start notification_server
